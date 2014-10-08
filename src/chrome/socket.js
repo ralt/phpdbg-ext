3a0e2e455e8dd0bc7ee16f751bcaddd9dd2af5bb
Florian MARGAINE <florian@margaine.com>
2014-10-08 22:57:51 +0200

    Sends a socket and start parsing xml... failure

--- src/chrome/socket.js
+++ src/chrome/socket.js
@@ -2,7 +2,15 @@
 
 var tcp = chrome.sockets.tcp;
 
-var socketId;
+var socketId, sendCb;
+
+tcp.onReceive.addListener(function(info) {
+    if (info.socketId !== socketId) return;
+
+    var parser = new DOMParser();
+    var xmlString = '<?xml version="1.0"?>\n' + ab2str(info.data);
+    sendCb(parser.parseFromString(xmlString, 'application/xml'));
+});
 
 exports.connect = function(hostname, port, cb) {
     tcp.create({}, function(socketInfo) {
@@ -10,3 +18,23 @@ exports.connect = function(hostname, port, cb) {
         tcp.connect(socketId, hostname, port || 0, cb);
     });
 };
+
+exports.send = function(command, cb) {
+    sendCb = cb;
+    tcp.send(socketId, str2ab(command + '\n'), function() {});
+};
+
+// Thanks html5rocks
+// http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint8Array(buf)); // ascii
+}
+
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length); // 1 bytes for each char, ascii
+    var bufView = new Uint8Array(buf);
+    for (var i = 0; i < str.length; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
--- src/chrome/views/debugger.js
+++ src/chrome/views/debugger.js
@@ -1,7 +1,25 @@
 'use strict';
 
+var socket = require('../socket');
+
 var name = 'debugger';
 var element = document.querySelector('#debugger');
+
+var connected = document.querySelector('#connected');
+var prompt = document.querySelector('#prompt');
+
+prompt.addEventListener('keyup', function(e) {
+    if (e.keyCode !== 13) return;
+
+    var command = prompt.value;
+    prompt.value = '';
+
+    socket.send(command, fillContent);
+});
+
+function fillContent(xmldata) {
+    console.log(xmldata);
+}
 
 module.exports = {
     getName: function() {
@@ -12,11 +30,7 @@ module.exports = {
         return element;
     },
 
-    start: start
+    start: function(hostname, port) {
+        connected.textContent = hostname + ':' + port;
+    }
 };
-
-var connected = document.querySelector('#connected');
-
-function start(hostname, port) {
-    connected.textContent = hostname + ':' + port;
-}
